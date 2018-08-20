1102598c343f23576b395d33a902c0fa8b39e4a1
Grzegorz Walewski <grzesiu2203@gmail.com>
2018-08-20 15:42:22 +0200

    Add new solution

--- czyJestPodzielna.py
+++ czyJestPodzielna.py
@@ -1,1 +1,11 @@
-int(input("Podaj liczbe asdasd"))
+podzielna=int(input("Podaj liczbe ktora chcesz podzielic\n"))
+dzielnik=int(input("Podaj liczbe przez ktora chcesz dzielic\n"))
+if dzielnik==0:
+	print("Pamietaj nie dziel przez ZERO!")
+	input()
+	quit()
+if podzielna%dzielnik>=1:
+	print("{} nie jest podzielna przez {}".format(podzielna,dzielnik))
+else:
+	wynik=int(podzielna/dzielnik)
+	print("Super! {} jest podzielna przez {}, a wynik tego dzielenia wynosi {}".format(podzielna,dzielnik,wynik))
